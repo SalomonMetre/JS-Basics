a8ddcff8d060bb09fe3fffb710cd24b95a3052d0
Salomon Metre <kulosmetros088@mgmail.com>
2022-04-19 20:47:12 +0300

    Basic JS programs

--- Basic Calculator/interaction.js
+++ Basic Calculator/interaction.js
@@ -1,19 +1,19 @@
 var number1 = document.getElementById('number1');
 var number2 = document.getElementById('number2');
 function add() {
-    var result = parseInt(number1.value) + parseInt(number2.value);
+    var result = parseFloat(number1.value) + parseFloat(number2.value);
     document.getElementById('result').value = result;
 }
 function subtract() {
-    var result = parseInt(number1.value) - parseInt(number2.value);
+    var result = parseFloat(number1.value) - parseFloat(number2.value);
     document.getElementById('result').value = result;
 }
 function multiply() {
-    var result = parseInt(number1.value) * parseInt(number2.value);
+    var result = parseFloat(number1.value) * parseFloat(number2.value);
     document.getElementById('result').value = result;
 }
 function divide() {
-    var result = parseInt(number1.value) / parseInt(number2.value);
+    var result = parseFloat(number1.value) / parseFloat(number2.value);
     document.getElementById('result').value = result;
 }
 function clearFields() {
